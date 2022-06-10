fb0cf206466ae5081094684db85225f405a4760a
Dimitar Dinchev <dinchev.dimitar@gmail.com>
2022-06-10 23:57:18 +0300

    Minimal AppsFlyer expo managed issue repro.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, ScrollView, Button } from "react-native"
+import { useEffect, useState } from "react"
+import appsFlyer from "react-native-appsflyer"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [gcd, setGcd] = useState('No GCD Yet...');
+  const [oaoa, setOaoa] = useState('No UDL Yet...');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    const gcdListener = appsFlyer.onInstallConversionData((res) => setGcd(JSON.stringify(res, null, 5)));
+    const oaoaListener = appsFlyer.onDeepLink((res) => setOaoa(JSON.stringify(res, null, 5)));
+
+    appsFlyer.initSdk({
+        isDebug: true,
+        devKey: 'Us4xXxXxXxQed',
+        onInstallConversionDataListener: true,
+        onDeepLinkListener: true,
+        timeToWaitForATTUserAuthorization: 5,
+        appId: '7xXxXxXx1',
+      },
+      (res) => console.log("AppsFlyer Success", res),
+      (err) => console.error("AppsFlyer Error", err)
+    )
+  }, [])
+
+  return (
+    <View style={[{ flex: 1, marginTop: 30 }]}>
+      <View style={[{ flex: 2 }]}>
+        <View style={{ backgroundColor: 'grey', alignItems: 'center' }}>
+          <Text>GCD</Text>
+        </View>
+        <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+          <Text style={[]}>{gcd}</Text>
+        </ScrollView>
+      </View>
+      <View style={[{ flex: 2 }]}>
+        <View style={{ backgroundColor: 'grey', alignItems: 'center' }}>
+          <Text>UDL</Text>
+        </View>
+        <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+          <Text style={[]}>{oaoa}</Text>
+        </ScrollView>
+      </View>
+      <View style={[{ flex: 1 }]}>
+        <Button title='Log Event' onPress={() =>
+          appsFlyer.logEvent("just_event", {
+            "param1": "foo",
+            "param2": 123,
+          })} />
+      </View>
+    </View>
+  )
+}
